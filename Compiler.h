8f3fd0a5c897c7770f4403abc21dd85c25eb493f
marymw <71140577+marymw@users.noreply.github.com>
2021-10-16 18:06:22 +0300

    Update Compiler.h

--- Compiler.h
+++ Compiler.h
@@ -1,6 +1,9 @@
-
+#ifndef COMP_H
+#define COMP_H
 
 int DefineCommand       (char *command, int *selectorPtr);
 int IsEqualCommand      (const char *firstCommand, const char *secondCommand);
 int DecomposeToCodeArray(MyString *indexPtr, int *codePtr, int numberOfStrings, int *instructionPtr);
 int PrintToCodeFile     (int *codePtr, int instructionPtr);
+
+#endif COMP_H
