36670900e51fcd2c0bba6d7ef8dcc809d6c68ecc
marymw <71140577+marymw@users.noreply.github.com>
2021-10-16 18:11:55 +0300

    Update FuncProcessor.cpp

--- FuncProcessor.cpp
+++ FuncProcessor.cpp
@@ -2,161 +2,95 @@
 #include "Processor.h"
 #include <assert.h>
 
-int GetCommand(char *command, double *paramPtr, int* typeOfCommandPtr){
+int DoCommand(Stack *someStackPtr, int *code, const int sizeOfFile){//type не нужен вроде
 
-	int statusOfScanf = scanf("%s %lf", command, paramPtr);
+	int instructionPtr = 0;
 
-	if (statusOfScanf == 0){
-		return COMMAND_ENTRY_ERROR;
-	}
+	while(instructionPtr <= sizeOfFile){
 
-	if (statusOfScanf == 1){
-		*typeOfCommandPtr = 1;
-	}
+		switch (code[instructionPtr]){
+			case -1: {
+				return 0;
+				break;
+			}
 
-	if (statusOfScanf == 2){
-		*typeOfCommandPtr = 2;
-	}
+			case CMD_HLT: {
+				return 0;
+				break;
+			}
 
-	//char a;
-	//int i = 0;
-	//for (i = 0; (a = getchar()) != EOF && a != '\n'; i++){
-	//	command[i] = a;
-	//}
-	//command[i] = '\0';
+			case CMD_PUSH: {
+				StackPush(someStackPtr, code[instructionPtr + 1]);
+				instructionPtr += 2;
+				break;
+			}
 
-	return NO_ERRORS;
-}
+			case CMD_POP: {
+				StackPop(someStackPtr);//пусть пока выбрасывает в никуда
+				instructionPtr += 1;
+				break;
+			}
 
-int DefineCommand(char *command, int *selectorPtr){
+			case CMD_ADD:{
+				double arg1 = StackPop(someStackPtr);
+				double arg2 = StackPop(someStackPtr);
+				StackPush(someStackPtr, arg1 + arg2);
+				instructionPtr += 1;
+				break;
+			}
 
-	if (IsEqualCommand(command, HLT)){
-		*selectorPtr = 1;
-		return NO_ERRORS;
-	}
-	if (IsEqualCommand(command, PUSH)){
-		*selectorPtr = 2;
-		return NO_ERRORS;
-	}
-	if (IsEqualCommand(command, POP)){
-		*selectorPtr = 3;
-		return NO_ERRORS;
-	}
-	if (IsEqualCommand(command, ADD)){
-		*selectorPtr = 4;
-		return NO_ERRORS;
-	}
-	if (IsEqualCommand(command, SUB)){
-		*selectorPtr = 5;
-		return NO_ERRORS;
-	}
-	if (IsEqualCommand(command, MULT)){
-		*selectorPtr = 6;
-		return NO_ERRORS;
-	}
-	if (IsEqualCommand(command, DIV)){
-		*selectorPtr = 7;
-		return NO_ERRORS;
-	}
-	if (IsEqualCommand(command, OUT)){
-		*selectorPtr = 8;
-		return NO_ERRORS;
-	}
-	if (IsEqualCommand(command, VER)){
-		*selectorPtr = 9;
-		return NO_ERRORS;
-	}
-	if (IsEqualCommand(command, DMP)){
-		*selectorPtr = 10;
-		return NO_ERRORS;
-	}
-	*selectorPtr = 0;
-	return NO_ERRORS;
-}
+			case CMD_SUB: {
+				double subtracter = StackPop(someStackPtr);//сначала вытащили вычитаемое
+				double minuend	  = StackPop(someStackPtr);
+				Type sub = minuend - subtracter;
+				StackPush(someStackPtr, sub);
+				instructionPtr += 1;
+				break;
+			}
 
+			case CMD_MULT: {
+				double multiplier1 = StackPop(someStackPtr);
+				double multiplier2 = StackPop(someStackPtr);
+				Type mult          =multiplier1 * multiplier2;
+				StackPush(someStackPtr, mult);
+				instructionPtr += 1;
+				break;
+			}
 
-int IsEqualCommand (const char *firstCommand, const char *secondCommand){
-	int i = 0;
-	for (i = 0; firstCommand[i] != '\0' && secondCommand[i] != '\0'; i++){
-		if(firstCommand[i] != secondCommand[i]){
-			return 0;
+			case CMD_DIV: {
+				double divisor = StackPop(someStackPtr);//сначала вытащили делитель
+				double dividend	  = StackPop(someStackPtr);
+				Type div        = dividend / divisor;
+				StackPush(someStackPtr, div);
+				instructionPtr += 1;
+				break;
+			}
+
+			case CMD_OUT: {
+				printf("Сейчас на вершине стека лежит %lf\n", StackTop(someStackPtr));
+				instructionPtr += 1;
+				break;
+			}
+
+			case CMD_VER: {
+				printf("Запущен верификатор!!\n");
+				ASSERT_OK(someStackPtr);
+				instructionPtr += 1;
+				break;
+			}
+
+			case CMD_DMP: {
+				printf("Запущен дамп!!\n");
+				StackDump(someStackPtr);
+				instructionPtr += 1;
+				break;
+			}
+			
+			default:{
+				printf("Неверная команда\n");
+				return -1;
+			}
 		}
-	}
-
-	if (firstCommand[i] == '\0' && secondCommand[i] == '\0'){
-		return 1;
-	}
-
-	return 0;
-}
-
-//!TODO обработка ошибок и что выводит
-int DoCommand(Stack *someStackPtr, int selector, int typeOfCommand, double param){//type не нужен вроде
-	switch (selector){
-
-		case 0: {
-			return 0;
-		}break;
-
-		case 1: {
-			return 0;
-		}break;
-
-		case 2: {
-			StackPush(someStackPtr, param);
-		}break;
-
-		case 3: {
-			StackPop(someStackPtr);//пусть пока выбрасывает в никуда
-		}break;
-
-		case 4:{
-			double arg1 = StackPop(someStackPtr);
-			double arg2 = StackPop(someStackPtr);
-			StackPush(someStackPtr, arg1 + arg2);
-		}break;
-
-		case 5: {
-			double subtracter = StackPop(someStackPtr);//сначала вытащили вычитаемое
-			double minuend	  = StackPop(someStackPtr);
-			Type sub = minuend - subtracter;
-			StackPush(someStackPtr, sub);
-		}break;
-
-		case 6: {
-			double multiplier1 = StackPop(someStackPtr);
-			double multiplier2 = StackPop(someStackPtr);
-			Type mult          =multiplier1 * multiplier2;
-			StackPush(someStackPtr, mult);
-		}break;
-
-		case 7: {
-			double divisor = StackPop(someStackPtr);//сначала вытащили делитель
-			double dividend	  = StackPop(someStackPtr);
-			Type div        = dividend / divisor;
-			StackPush(someStackPtr, div);
-		}break;
-
-		case 8: {
-			printf("Сейчас на вершине стека лежит %lf\n", StackTop(someStackPtr));
-		}break;
-
-		case 9: {
-			printf("Запущен верификатор!!\n");
-			ASSERT_OK(someStackPtr);
-		}break;
-
-		case 10: {
-			printf("Запущен дамп!!\n");
-			StackDump(someStackPtr);
-		}break;
-
-		default:{
-			;
-		}
-
-
-
 	}
 	return 0;
 }
