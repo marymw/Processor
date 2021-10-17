8bd1e214a4ee6382a9f0e5b0a31fae459dae122a
marymw <71140577+marymw@users.noreply.github.com>
2021-10-17 11:50:00 +0300

    Update MainProcessor.cpp
    работает с неравномерным бинарным файлом

--- MainProcessor.cpp
+++ MainProcessor.cpp
@@ -2,60 +2,33 @@
 #include "Processor.h"
 #include "Lifo.h"
 #include <stdlib.h>
-
-
-
+#include "HeadOneg.h"
 
 int main(int argc, char *argv[]){
 
 	Stack MyStack = {};
+
 	StackCtor(&MyStack, 10);
 
+	char *CODE_FILE = argv[1];
 
-	//char *INPUT_FILE = argv[1];
+	FILE *codeFile = fopen(CODE_FILE, "rb");
 
-	//FILE *inputFile = fopen(INPUT_FILE, "r");
-
-	char command[LEN_OF_COMMAND] = {};
-	double param = 0;
-	int typeOfCommand = 0;
-
-	//char  *command = (char *)calloc(LEN_OF_COMMAND, sizeof(char));
-
-	int selector = 1;
-
-	do {
-		int statusOfGetCommand = GetCommand(command, &param, &typeOfCommand);
-
-		if (statusOfGetCommand != 0){
-			printf("Ошибка введения команды!\n");
-			return -1;
-		}
-
-		int statusOfDefineCommand = DefineCommand(command, &selector);
-
-		if (statusOfDefineCommand != 0){
-			printf("Ошибка определения команды!Вы ввели неверную команду!\n");
-			return -1;
-		}
-
-		printf("command selector is %d\n", selector);
-		printf("type of command is %d\n", typeOfCommand);
-
-		int statusOfDoCommand = DoCommand(&MyStack, selector, typeOfCommand, param);
-		if (statusOfDoCommand != 0){
-			printf("Ошибка выполнения команды!\n");
-			return -1;
-		}
-
-		//printf("%s\n", command);
-
-	}while (selector != 1);
-	printf("Я устал, я мухожук... \n");//
-	//}
+	char *code = (char *)calloc(100, sizeof(int));
 	
 
-	//fclose(inputFile);
+	int sizeOfFile = GetSizeOfFile(codeFile);
+	
+	fread(code, sizeof(char), sizeOfFile, codeFile);
+	int statusOfDoCommand = DoCommand(&MyStack, code, sizeOfFile);
+	if (statusOfDoCommand != 0){
+		printf("Ошибка выполнения команды!\n");
+		return -1;
+	}
+
+	printf("Я устал, я мухожук... \n");
+	free(code);
+	fclose(codeFile);
 	StackDtor(&MyStack);
 
 	return 0;
