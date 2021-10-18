4c1dd4e42be42c29849649336bd62db46f48800a
marymw <71140577+marymw@users.noreply.github.com>
2021-10-19 01:08:45 +0300

    Update Compiler.h
    corrections with error handing

--- Compiler.h
+++ Compiler.h
@@ -1,9 +1,35 @@
 #ifndef COMP_H
 #define COMP_H
 
-int DefineCommand       (char *command, int *selectorPtr);
-int IsEqualCommand      (const char *firstCommand, const char *secondCommand);
-int DecomposeToCodeArray(MyString *indexPtr, int *codePtr, int numberOfStrings, int *instructionPtr);
-int PrintToCodeFile     (int *codePtr, int instructionPtr);
+#define CheckNullPtr(param, string, nameOfError) do {			\
+										                                if (param == nullptr) {  \
+										                                     	printf(string);		  \
+											                                    return nameOfError;	\
+									                                 	}						              \
+								                               	} while(0)
+
+#define CheckNull(param, string, nameOfError) do {				       \
+											                        	if (param != 0) {	   \
+												                      	    printf(string);    \
+													                          return nameOfError;\
+											                        	}				       \
+								 			                        } while(0)
+
+#define CheckEqual(param1, param2, string, nameOfError) do {				       \
+															if (param1 != param2) {\
+																printf(string);    \
+																return nameOfError;\
+															}				       \
+								 			  			} while(0)
+
+#define CheckNoNull(param, string, nameOfError) do {				       \
+													if (param == 0) {	   \
+														printf(string);    \
+														return nameOfError;\
+													}				       \
+								 			    } while(0)
+
+int DecomposeToCodeArray(MyString *indexPtr, char *codePtr, const int numberOfStrings, int *instructionPtr, int *typeOfCommand);
+int PrintToCodeFile     (const char *codePtr, const int instructionPtr);
 
 #endif COMP_H
