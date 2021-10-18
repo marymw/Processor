ac33f1a6a3f204fe23fb4f67c190bec75141e10f
marymw <71140577+marymw@users.noreply.github.com>
2021-10-19 01:12:40 +0300

    Update Compiler.h

--- Compiler.h
+++ Compiler.h
@@ -1,34 +1,6 @@
 #ifndef COMP_H
 #define COMP_H
 
-#define CheckNullPtr(param, string, nameOfError) do {			\
-										                                if (param == nullptr) {  \
-										                                     	printf(string);		  \
-											                                    return nameOfError;	\
-									                                 	}						              \
-								                               	} while(0)
-
-#define CheckNull(param, string, nameOfError) do {				       \
-											                        	if (param != 0) {	   \
-												                      	    printf(string);    \
-													                          return nameOfError;\
-											                        	}				       \
-								 			                        } while(0)
-
-#define CheckEqual(param1, param2, string, nameOfError) do {				       \
-															if (param1 != param2) {\
-																printf(string);    \
-																return nameOfError;\
-															}				       \
-								 			  			} while(0)
-
-#define CheckNoNull(param, string, nameOfError) do {				       \
-													if (param == 0) {	   \
-														printf(string);    \
-														return nameOfError;\
-													}				       \
-								 			    } while(0)
-
 int DecomposeToCodeArray(MyString *indexPtr, char *codePtr, const int numberOfStrings, int *instructionPtr, int *typeOfCommand);
 int PrintToCodeFile     (const char *codePtr, const int instructionPtr);
 
