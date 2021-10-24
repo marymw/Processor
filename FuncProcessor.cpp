dd32eccd5975c76e09e8d869f25ac9ee454dd4df
marymw <71140577+marymw@users.noreply.github.com>
2021-10-24 15:54:28 +0300

    Update FuncProcessor.cpp

--- FuncProcessor.cpp
+++ FuncProcessor.cpp
@@ -4,32 +4,60 @@
 
 
 int ExecuteCommand(Processor *SomeProcessorPtr, const int sizeOfFile){
-
+	printf("я в функции %s на строчке %d\n", __FUNCTION__, __LINE__);
 	while(SomeProcessorPtr->instructionPtr <= sizeOfFile){
-
-		switch (*(SomeProcessorPtr->code + SomeProcessorPtr->instructionPtr)){
-
+		printf("я в функции %s на строчке %d\n", __FUNCTION__, __LINE__);
+		char command = (*(SomeProcessorPtr->code + SomeProcessorPtr->instructionPtr)) & 31;
+		char typeOfCommand = (*(SomeProcessorPtr->code + SomeProcessorPtr->instructionPtr));
+		switch (command){
+			printf("я в функции %s на строчке %d\n", __FUNCTION__, __LINE__);
 			case CMD_HLT: {
+				printf("я в функции %s на строчке %d\n", __FUNCTION__, __LINE__);
 				return NO_ERRORS;
 			}
-
+		
 			case CMD_PUSH: {
-				StackPush(&(SomeProcessorPtr->stackOfProc), *((int *)(SomeProcessorPtr->code + SomeProcessorPtr->instructionPtr + 1)));
-				SomeProcessorPtr->instructionPtr += 5;
+				printf("я в функции %s на строчке %d\n", __FUNCTION__, __LINE__);
+				
+				int argument = 0;
+				SomeProcessorPtr->instructionPtr++;
+
+				if (typeOfCommand & IMM_CONST){
+					printf("я в функции %s на строчке %d\n", __FUNCTION__, __LINE__);
+					argument += *((int *)(SomeProcessorPtr->code + SomeProcessorPtr->instructionPtr));
+					SomeProcessorPtr->instructionPtr += 4;
+				}
+				if (typeOfCommand & REG_CONST){
+					printf("я в функции %s на строчке %d\n", __FUNCTION__, __LINE__);
+					printf("SomeProcessorPtr->instructionPtr = %d\n", SomeProcessorPtr->instructionPtr);
+					int num = *(((SomeProcessorPtr->code + SomeProcessorPtr->instructionPtr)));
+					SomeProcessorPtr->instructionPtr++;
+					printf("num = %d\n", num);//тут какой-то мусор
+					argument += SomeProcessorPtr->REGS[ num ];
+				}
+				if (typeOfCommand & RAM_CONST){
+					printf("я в функции %s на строчке %d\n", __FUNCTION__, __LINE__);
+					argument += SomeProcessorPtr->RAM[argument];
+				}
+				printf("я в функции %s на строчке %d\n", __FUNCTION__, __LINE__);
+				StackPush(&(SomeProcessorPtr->stackOfProc), argument);
 				break;
 			}
 
 			case CMD_POP: {
+				printf("я в функции %s на строчке %d\n", __FUNCTION__, __LINE__);
 				if (SomeProcessorPtr->stackOfProc.stackSize <= 0){
 					printf("Чисел в стеке больше нет!\n");
 					return 0;
 				}
-				StackPop(&(SomeProcessorPtr->stackOfProc));//пусть пока выбрасывает в никуда
 				SomeProcessorPtr->instructionPtr += 1;
+				SomeProcessorPtr->REGS[(*(SomeProcessorPtr->code + SomeProcessorPtr->instructionPtr++))] = StackPop(&(SomeProcessorPtr->stackOfProc));
+				
 				break;
 			}
 
 			case CMD_ADD:{
+				printf("я в функции %s на строчке %d\n", __FUNCTION__, __LINE__);
 				double arg1 = StackPop(&(SomeProcessorPtr->stackOfProc));
 				double arg2 = StackPop(&(SomeProcessorPtr->stackOfProc));
 				StackPush(&(SomeProcessorPtr->stackOfProc), arg1 + arg2);
@@ -38,6 +66,7 @@ int ExecuteCommand(Processor *SomeProcessorPtr, const int sizeOfFile){
 			}
 
 			case CMD_SUB: {
+				printf("я в функции %s на строчке %d\n", __FUNCTION__, __LINE__);
 				double subtracter = StackPop(&(SomeProcessorPtr->stackOfProc));//сначала вытащили вычитаемое
 				double minuend	  = StackPop(&(SomeProcessorPtr->stackOfProc));
 				Type sub = minuend - subtracter;
@@ -47,6 +76,7 @@ int ExecuteCommand(Processor *SomeProcessorPtr, const int sizeOfFile){
 			}
 
 			case CMD_MUL: {
+				printf("я в функции %s на строчке %d\n", __FUNCTION__, __LINE__);
 				double multiplier1 = StackPop(&(SomeProcessorPtr->stackOfProc));
 				double multiplier2 = StackPop(&(SomeProcessorPtr->stackOfProc));
 				Type mul          = multiplier1 * multiplier2;
@@ -56,6 +86,7 @@ int ExecuteCommand(Processor *SomeProcessorPtr, const int sizeOfFile){
 			}
 
 			case CMD_DIV: {
+				printf("я в функции %s на строчке %d\n", __FUNCTION__, __LINE__);
 				double divisor  = StackPop(&(SomeProcessorPtr->stackOfProc));//сначала вытащили делитель
 				double dividend	= StackPop(&(SomeProcessorPtr->stackOfProc));
 				Type div        = dividend / divisor;
@@ -65,6 +96,7 @@ int ExecuteCommand(Processor *SomeProcessorPtr, const int sizeOfFile){
 			}
 
 			case CMD_OUT: {
+				printf("я в функции %s на строчке %d\n", __FUNCTION__, __LINE__);
 				if (SomeProcessorPtr->stackOfProc.stackSize <= 0){
 					printf("Чисел в стеке больше нет!\n");
 					return 0;
@@ -75,6 +107,7 @@ int ExecuteCommand(Processor *SomeProcessorPtr, const int sizeOfFile){
 			}
 
 			case CMD_VER: {
+
 				printf("Запущен верификатор!!\n");
 				ASSERT_OK(&(SomeProcessorPtr->stackOfProc));
 				SomeProcessorPtr->instructionPtr += 1;
@@ -90,17 +123,20 @@ int ExecuteCommand(Processor *SomeProcessorPtr, const int sizeOfFile){
 			}
 
 			case CMD_IN: {
+				printf("я в функции %s на строчке %d\n", __FUNCTION__, __LINE__);
 				printf("Введите число, которое хотите записать в стек\n");
 				SomeProcessorPtr->instructionPtr += 1;
 				double number = 0;
-				int statusOfScanf = scanf("%d", &number);
+				int statusOfScanf = scanf("%lf", &number);
 				//почему эта
+				printf("я в функции %s на строчке %d\n", __FUNCTION__, __LINE__);
 				while (statusOfScanf != 1) {
 					printf("Вы ввели откровенный бред. Попробуйте снова.");
-					statusOfScanf = scanf("%d", &number);
+					statusOfScanf = scanf("%lf", &number);
 				}
-
+				printf("я в функции %s на строчке %d\n", __FUNCTION__, __LINE__);
 				StackPush(&(SomeProcessorPtr->stackOfProc), number);
+				printf("я в функции %s на строчке %d\n", __FUNCTION__, __LINE__);
 				break;
 			}
 			
