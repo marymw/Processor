49bd75391ae916fd4075ccdf95d4bbe92828f60f
marymw <71140577+marymw@users.noreply.github.com>
2021-10-24 15:50:22 +0300

    Update Compiler.h

--- Compiler.h
+++ Compiler.h
@@ -1,7 +1,9 @@
 #ifndef COMP_H
 #define COMP_H
 
-int DecomposeToCodeArray(MyString *indexPtr, char *codePtr, const int numberOfStrings, int *instructionPtr, int *typeOfCommand);
+
+
+int DecomposeToCodeArray(MyString *indexPtr, char *codePtr, const int numberOfStrings, int *instructionPtr, char *typeOfCommand);
 int PrintToCodeFile     (const char *codePtr, const int instructionPtr);
 
 #endif COMP_H
