16078614d80b6e262c6f6c99a6b1aece846ab8c8
marymw <71140577+marymw@users.noreply.github.com>
2021-11-01 16:36:02 +0300

    Update Compiler.h

--- Compiler.h
+++ Compiler.h
@@ -1,9 +1,26 @@
 #ifndef COMP_H
 #define COMP_H
 
+const int MAX_NUM_OF_LABELS = 10;
+const int MAX_LEN_OF_LABEL  = 10;
+
+struct label {
+	int   address;
+	char *name;
+};
 
 
-int DecomposeToCodeArray(MyString *indexPtr, char *codePtr, const int numberOfStrings, int *instructionPtr, char *typeOfCommand);
+
+// #define CURCOMMAND(arg) LEN_OF_CMD_arg
+//#define CMD(arg) CMD_arg
+
+// #define CHECK_COMMAND(commandToCheck) if (IsEqualCommand(command, commandToCheck, CURCOMMAND(commandToCheck + 1)){\
+// 	 										CheckNull(typeOfCommand, "Ввёл что-то не то!\n", LACK_OF_ARGUMENTS);\
+// 	 										*selectorPtr = COM(arg);\
+// 	 										return NO_ERRORS;\
+// 	 									}
+
+int DecomposeToCodeArray(label **arrayOfLabels, MyString *indexPtr, char *codePtr, const int numberOfStrings, int *instructionPtr, char *typeOfCommand);
 int PrintToCodeFile     (const char *codePtr, const int instructionPtr);
 
-#endif COMP_H
+#endif 
