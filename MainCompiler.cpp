cf5771dce4a115ae465a256db6c673396ffbe602
marymw <71140577+marymw@users.noreply.github.com>
2021-11-04 21:02:31 +0300

    Update MainCompiler.cpp

--- MainCompiler.cpp
+++ MainCompiler.cpp
@@ -4,7 +4,6 @@
 #include "Compiler.h"
 #include <stdlib.h>
 
-//выделить больше под адрес
 int main(int argc, char *argv[]) {
 
 	CheckEqual(argc, 2, "No instruction file!\n", NO_FILE);
@@ -28,8 +27,8 @@ int main(int argc, char *argv[]) {
 	char typeOfCommand   = 0;
 	label *arrayOfLabels = (label *)calloc(MAX_NUM_OF_LABELS, sizeof(label));
 	
-	for (int i = 0; i < 10; i++){
-		arrayOfLabels[i].name = (char *)calloc(10, sizeof(char));
+	for (int i = 0; i < MAX_NUM_OF_LABELS; i++){
+		arrayOfLabels[i].name = (char *)calloc(MAX_LEN_OF_LABEL, sizeof(char));
 		arrayOfLabels[i].address = -1;
 	}
  
@@ -46,15 +45,19 @@ int main(int argc, char *argv[]) {
 	statusDecomposeToCodeArray = DecomposeToCodeArray(arrayOfLabels, index, code, numberOfStrings, &instructionPtr, &typeOfCommand);
 	CheckNull(statusDecomposeToCodeArray, "Failed to decompose into code array\n", NULL_PTR_ERROR);
 	PRINT_LOG();
-	for (int i = 0; i < 10; i++){
+	#ifdef DEBUG5
+	for (int i = 0; i < MAX_NUM_OF_LABELS; i++){
 		printf("%d %s %d\n", i, arrayOfLabels[i].name, arrayOfLabels[i].address);
 	}
+	#endif
 	
-
 	int statusPrintToCodeFile = PrintToCodeFile(code, instructionPtr);
 	CheckNull(statusPrintToCodeFile, "Failed to print the code array into fail\n", NULL_PTR_ERROR);
 	
 	free(code);
+	for (int i = 0; i < MAX_NUM_OF_LABELS; i++){
+		free(arrayOfLabels[i].name);
+	}
 	free(index);
 	free(buffer);
 	fclose(asmFile);
